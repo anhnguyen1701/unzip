eca61ceda139d67dfeba0905c04859880743eef6
anh nguyen <nguyenlamanh1701@gmail.com>
2022-04-02 12:57:17 +0700

    decompress

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+/*
+     decompress work! 
+*/
+
+// const decompress = require('decompress');
+// const path = require('path');
+
+// (async () => {
+//     try {
+//         const files = await decompress('unicorn.zip', 'dist', {
+//             filter: (file) => path.extname(file.path) !== '.exe',
+//         });
+//         console.log(files);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })();
